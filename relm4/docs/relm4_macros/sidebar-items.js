ef3a3437749b0a298c8c8ba30d373f5b9dc65b30
AaronErhardt <AaronErhardt@users.noreply.github.com>
2022-01-13 02:04:08 +0000

    deploy: a05d5c6ad78073f42927ad8d224e568cd16131f4

--- relm4/docs/relm4_macros/sidebar-items.js
+++ relm4/docs/relm4_macros/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"attr":[["widget","Macro that implemements relm4::Widgets and generates the corresponding struct."]],"derive":[["Components",""]]});
+initSidebarItems({"attr":[["widget","Macro that implemements relm4::Widgets and generates the corresponding struct."]],"derive":[["Components",""]],"macro":[["menu",""]]});
